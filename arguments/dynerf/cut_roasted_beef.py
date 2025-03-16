fb92c69a1ce5e3e306718d8b53b9fa853b3a5927
AAAma <58819382+WuJH2001@users.noreply.github.com>
2025-03-16 09:39:57 +0800

    Update cut_roasted_beef.py

--- arguments/dynerf/cut_roasted_beef.py
+++ arguments/dynerf/cut_roasted_beef.py
@@ -1,7 +1,6 @@
 _base_ = './default.py'
-OptimizationParams = dict( # 10000 轮达到最大·可以尝试设置1w
+OptimizationParams = dict( 
     hash_init_lr = 0.0002 ,
     hash_final_lr = 0.00002 ,
     hashmap_size = 19 ,
-    # opacity_reset_interval = 3000 ,
 )
