cae96a023c5cdec6ca77769d5346fe94c08625f6
AAAma <58819382+WuJH2001@users.noreply.github.com>
2025-03-17 13:41:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #  scenes = [ "flame_salmon", "coffee_martini", "cook_spinach","cut_roasted_beef","flame_steak", "sear_steak"]
 
-python train.py -s /datasets/neural_3D/flame_salmon_down2_4dgs --expname  dynerf/flame_salmon_down2_4dgs --configs arguments/dynerf_no_seg/flame_salmon.py 
+python train.py -s /datasets/neural_3D/flame_salmon --expname  dynerf/flame_salmon --configs arguments/dynerf/flame_salmon.py 
 
 
 
