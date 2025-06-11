c523fbc0bfc0e1a1c707d14a6588585a0d61c456
Bona Park <jaeyeon.park@hcs.snu.ac.kr>
2025-06-11 13:50:32 +0900

    Add missing msssim function (IDENTICAL to ssim)

--- utils/loss_utils.py
+++ utils/loss_utils.py
@@ -43,6 +43,17 @@ def ssim(img1, img2, window_size=11, size_average=True):
 
     return _ssim(img1, img2, window, window_size, channel, size_average)
 
+
+def msssim(img1, img2, window_size=11, size_average=True):
+    channel = img1.size(-3)
+    window = create_window(window_size, channel)
+
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
+
+    return _ssim(img1, img2, window, window_size, channel, size_average)
+
 def _ssim(img1, img2, window, window_size, channel, size_average=True):
     mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
     mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
